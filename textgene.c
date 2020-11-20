04ac5cc916bb2e1b97f9e263a4051b1394ab7012
ManatoYamaguchi <manato06452@gmail.com>
2020-11-20 23:42:03 +0900

    Add a file

---
+++ textgene.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <ctype.h>
+ 
+int main()
+{
+	FILE *lf,*sf;
+	int c;
+ 
+	if(!(lf=fopen("inalph.txt","r")) || (!(sf=fopen("outalph.txt","w"))))
+	{
+		printf(" ファイルオープンエラー\n");
+		return -1;
+	}
+ 
+	while((c=fgetc(lf))!=EOF)
+	{
+	
+		if(isalpha(c)){
+			fputc(c,sf);
+		} else if(c == ' ') {
+			fputc(c,sf);
+		} else if(c == '\n') {
+			fputc(c,sf);
+		} else {
+			fputc(' ',sf);
+		}
+	}
+ 
+	fclose(sf);
+	fclose(lf);
+ 
+	return 0;
+}
